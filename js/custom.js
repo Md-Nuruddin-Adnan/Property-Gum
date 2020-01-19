a00a312b044643e52c0e97d9aeb380837af56c7e
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2020-01-19 20:21:10 +0600

    ProperpyGum

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+//property card slider
+
+$('.slider').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  
+  ]
+});
